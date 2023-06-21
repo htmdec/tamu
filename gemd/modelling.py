4dfb04afa7e850f21d9977ac919e27f1a3afe938
arachid1 <alilerachidi@gmail.com>
2023-06-21 12:07:41 -0400

    finished up plotting all graph elements

--- gemd/modelling.py
+++ gemd/modelling.py
@@ -7,17 +7,20 @@ from workflow.TAMUWorkflow import TAMUWorkflow
 from workflow.Workflow import Workflow
 from utilities.tools import plot_graph
 import os
+import pathlib
 from pathlib import Path
 
 
 def main():
+    _file_path = pathlib.Path(__file__)
     iteration_count = 2
     iteration = "AAB"
-    source_folder = "../data/"
-    output_folder = "data/{}".format(iteration)
-    sample_data_folder = "../Sample Data/Iteration{}_{}".format(
+    source_folder = _file_path.parent.parent / "data/"
+    output_folder = _file_path.parent / "data/{}".format(iteration)
+    sample_data_folder = _file_path.parent.parent / "Sample Data/Iteration{}_{}".format(
         iteration_count, iteration
     )
+
     path = os.path.join(source_folder, iteration)
     w_aab = TAMUWorkflow(
         path,
@@ -27,6 +30,14 @@ def main():
     )
     w_aab.testing_mode = True
     w_aab.build_model()
+    # print(w_aab.displayable())
+    w_aab.thin_dumps()
+
+    plot_graph("/srv/hemi01-j01/htmdec/birdshot/gemd/data/AAB/AAB01/VAM/A/thin_jsons")
+    plot_graph(
+        "/srv/hemi01-j01/htmdec/birdshot/gemd/data/AAB/AAB01/VAM/A/thin_jsons",
+        obj_state="spec",
+    )
 
 
 if __name__ == "__main__":
